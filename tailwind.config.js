a163d8be8287433dac15a0cf5c60f43ba242cc9e
Rodion Sibov <rodionsibov@gmail.com>
2021-06-16 17:46:23 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    enabled: false,
+    content: ['./public/**/*.html']
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
