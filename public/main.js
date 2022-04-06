45432ece15611e9011ed5ceb10406637a379e9ba
Rodion Sibov <rodionsibov@gmail.com>
2022-04-06 17:28:13 +0300

    add footer with github link

--- public/main.js
+++ public/main.js
@@ -1,54 +1,56 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            idx: 0,
-            selectedAnswer: '',
-            correctAnswers: 0,
-            wrongAnswers: 0,
-            count: 3,
-            questions: [
-                {
-                    question: 'Rolex is a company that specializes in what type of product?',
-                    answers: { a: 'Bags', b: 'Watches', c: 'Shoes', d: 'Laptops' },
-                    correctAnswer: 'b'
-                },
-                {
-                    question: 'When did Facebook launch?',
-                    answers: { a: '2005', b: '2008', c: '2003', d: '2004' },
-                    correctAnswer: 'd'
-                },
-                {
-                    question: 'Albert Einstein had trouble with mathematics when he was in school?',
-                    answers: { a: 'True', b: 'False' },
-                    correctAnswer: 'b'
-                },
-            ]
-        }
+  data() {
+    return {
+      idx: 0,
+      selectedAnswer: "",
+      correctAnswers: 0,
+      wrongAnswers: 0,
+      count: 3,
+      questions: [
+        {
+          question:
+            "Rolex is a company that specializes in what type of product?",
+          answers: { a: "Bags", b: "Watches", c: "Shoes", d: "Laptops" },
+          correctAnswer: "b",
+        },
+        {
+          question: "When did Facebook launch?",
+          answers: { a: "2005", b: "2008", c: "2003", d: "2004" },
+          correctAnswer: "d",
+        },
+        {
+          question:
+            "Albert Einstein had trouble with mathematics when he was in school?",
+          answers: { a: "True", b: "False" },
+          correctAnswer: "b",
+        },
+      ],
+    };
+  },
+  methods: {
+    answered(e) {
+      this.selectedAnswer = e.target.value;
+      if (this.selectedAnswer == this.questions[this.idx].correctAnswer) {
+        this.correctAnswers++;
+      } else {
+        this.wrongAnswers++;
+      }
     },
-    methods: {
-        answered(e) {
-            this.selectedAnswer = e.target.value
-            if (this.selectedAnswer == this.questions[this.idx].correctAnswer) {
-                this.correctAnswers++
-            } else {
-                this.wrongAnswers++
-            }
-        },
-        nextQuestion() {
-            this.idx++
-            this.selectedAnswer = ''
-            document.querySelectorAll('input').forEach(el => el.checked = false)
-        },
-        showResults() {
-            this.idx++
-        },
-        resetQuiz() {
-            this.idx = 0
-            this.selectedAnswer = ''
-            this.correctAnswers = 0
-            this.wrongAnswers = 0
-        }
-    }
-})
+    nextQuestion() {
+      this.idx++;
+      this.selectedAnswer = "";
+      document.querySelectorAll("input").forEach((el) => (el.checked = false));
+    },
+    showResults() {
+      this.idx++;
+    },
+    resetQuiz() {
+      this.idx = 0;
+      this.selectedAnswer = "";
+      this.correctAnswers = 0;
+      this.wrongAnswers = 0;
+    },
+  },
+});
 
-app.mount('#app')
+app.mount("#app");
